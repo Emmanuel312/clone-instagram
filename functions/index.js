f5ada5a3e9c9d6208b32087b8f87151f161a176f
paulo jorge <paulojorge81@hotmail.com>
2019-05-16 18:47:35 -0300

    finish cloudstorage

--- functions/index.js
+++ functions/index.js
@@ -2,15 +2,40 @@ const functions = require('firebase-functions');
 const cors = require('cors')({origin: true})
 const fs = require('fs')
 const uuid = require('uuid-v4')
-const { Storage } = require('@google-cloud/firestore')
-const storage = new Storage({projectId: fir-app-43252,keyFilename:'fileName.json'})
+const { Storage } = require('@google-cloud/storage')
+const storage = new Storage({projectId: 'fir-app-43252',keyFilename:'fileName.json'})
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.uploadImage = functions.https.onRequest((request, response) =>
-{
-    cors(req,res, () =>
-    {
-        
+exports.uploadImage = functions.https.onRequest((request, response) => {
+    cors(request, response, () => {
+        try {
+            fs.writeFileSync('/tmp/imageToSave.jpg', 
+                request.body.image, 'base64')
+            
+            const bucket = storage.bucket('fir-app-43252.appspot.com/')
+            const id = uuid()
+            bucket.upload('/tmp/imageToSave.jpg', {
+                uploadType: 'media',
+                destination: `/posts/${id}.jpg`,
+                metadata: {
+                    metadata: {
+                        contentType: 'image/jpeg',
+                        firebaseStorageDownloadTokens: id
+                    }
+                }
+            }, (err, file) => {
+                if (err) {
+                    return response.status(500).json({ error: err })
+                } else {
+                    const fileName = encodeURIComponent(file.name)
+                    const imageUrl = 'https://firebasestorage.googleapis.com/v0/b/'
+                        + bucket.name + '/o/' + fileName + '?alt=media&token=' + id
+                    return response.status(201).json({ imageUrl: imageUrl })
+                }
+            })
+        } catch (err) {
+            console.log(err)
+            return response.status(500).json({ error: err })
+        }
     })
 });
